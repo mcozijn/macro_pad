a1841cf77804f4553cc9862d557f6a8fb12354cd
Boyan <boyan@confest.im>
2024-11-24 23:42:01 +0100

    Normal test config

--- src/config.h
+++ src/config.h
@@ -17,10 +17,10 @@ static const uint col_pins[MATRIX_COLS] = {3, 4, 5};
 static const uint row_pins[MATRIX_ROWS] = {6, 7, 8, 9};
 
 static const uint8_t keymap[MATRIX_ROWS][MATRIX_COLS] = {
-    {HID_KEY_A, HID_KEY_M, HID_KEY_O},
-    {HID_KEY_G, HID_KEY_U, HID_KEY_S},
-    {HID_KEY_S, HID_KEY_U, HID_KEY_S},
-    {HID_KEY_CAPS_LOCK, HID_KEY_ENTER, HID_KEY_BACKSPACE},
+    {HID_KEY_A, HID_KEY_B, HID_KEY_C},
+    {HID_KEY_D, HID_KEY_E, HID_KEY_F},
+    {HID_KEY_G, HID_KEY_H, HID_KEY_I},
+    {HID_KEY_J, HID_KEY_K, HID_KEY_L},
 };
 
 static const EncoderControls encoder = {HID_USAGE_CONSUMER_VOLUME_INCREMENT, HID_USAGE_CONSUMER_VOLUME_DECREMENT, HID_USAGE_CONSUMER_PLAY_PAUSE};
