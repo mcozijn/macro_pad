019237f39fd33d2b2863528c60d9803529d88a0c
Boyan <boyan@confest.im>
2024-11-27 03:58:29 +0100

    annotated config

--- src/config.h
+++ src/config.h
@@ -16,6 +16,8 @@ typedef struct EncoderControls {
 static const uint col_pins[MATRIX_COLS] = {3, 4, 5};
 static const uint row_pins[MATRIX_ROWS] = {6, 7, 8, 9};
 
+// pico-sdk/lib/tinyusb/src/class/hid/hid.h to see the keycodes
+// Simple keymap example
 static const uint8_t keymap[MATRIX_ROWS][MATRIX_COLS] = {
     {HID_KEY_A, HID_KEY_B, HID_KEY_C},
     {HID_KEY_D, HID_KEY_E, HID_KEY_F},
@@ -23,4 +25,5 @@ static const uint8_t keymap[MATRIX_ROWS][MATRIX_COLS] = {
     {HID_KEY_J, HID_KEY_K, HID_KEY_L},
 };
 
+// Simple encoder example
 static const EncoderControls encoder = {HID_USAGE_CONSUMER_VOLUME_INCREMENT, HID_USAGE_CONSUMER_VOLUME_DECREMENT, HID_USAGE_CONSUMER_PLAY_PAUSE};
